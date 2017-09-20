c72d04e966e5b65f5bb18cb80120184ff1da8dd0
maria thomas, paul guevarra and ryan nunez <ryan@>
2017-09-20 16:48:33 -0700

    include separate child classes of edit, complete, new

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,14 @@ import { Component } from '@angular/core';
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <br>
-    <ul>
-         <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
-    </ul>
+    <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+    <!-- <ul>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+     </ul>-->
     <hr>
-     <div *ngIf="selectedTask">
+    <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+    <new-task (newTaskSender)="addTask($event)"></new-task>
+    <!-- <div *ngIf="selectedTask">
        <h3>{{selectedTask.description}}</h3>
        <p>Task Complete? {{selectedTask.done}}</p>
        <h3>Edit Task</h3>
@@ -23,7 +27,7 @@ import { Component } from '@angular/core';
        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
        <button (click)="finishedEditing()">Done</button>
-    </div>
+    </div>-->
   </div>
   `
 })
@@ -34,40 +38,45 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course',3),
-    new Task('Begin brainstorming possible JavaScript group projects',2),
-    new Task('Add README file to last few Angular repos on GitHub',2)
-    ];
-    selectedTask = null;
+  selectedTask = null;
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
 
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
+
+  // priorityColor(currentTask){
+  //   if (currentTask.priority === 3){
+  //     return "bg-danger";
+  //   } else if (currentTask.priority === 2) {
+  //     return  "bg-warning";
+  //   } else {
+  //     return "bg-info";
+  //   }
+  // }
 
   editTask(clickedTask) {
   this.selectedTask = clickedTask;
-}
+  }
 
   finishedEditing() {
     this.selectedTask = null;
   }
 
-  isDone(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done!");
-    } else {
-      alert("This task is not done. Better get to work!");
-    }
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
   }
+
+  // isDone(clickedTask: Task) {
+  //   if(clickedTask.done === true) {
+  //     alert("This task is done!");
+  //   } else {
+  //     alert("This task is not done. Better get to work!");
+  //   }
+  // }
 }
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public priority: number) {   }
-}
+// export class Task {
+//   public done: boolean = false;
+//   constructor(public description: string, public priority: number) {   }
+// }
